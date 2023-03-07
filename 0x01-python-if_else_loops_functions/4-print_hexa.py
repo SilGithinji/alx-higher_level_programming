cc9f7809a162cd64331b64de5fe6f764995bf052
SilGithinji <isaacgithinji4612@gmail.com>
2023-03-07 03:29:38 -0800

    A program that prints all numbers from 0 to 98 in decimal and in hexadecimal

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for m in range(0, 99):
-    print("{} = {}".format(m, hex(m)))
+# print 0 to 98 in both hexadecimal and decimal
+for hexDec in range(0, 99):
+    print("{} = {}".format(hexDec, hex(hexDec)))
