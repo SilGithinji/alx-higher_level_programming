d94b9c4c6be7ad86bde6b547d2f86662ade30541
SilGithinji <isaacgithinji4612@gmail.com>
2023-03-08 09:06:15 -0800

    4. Hexadecimal printing

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# print 0 to 98 in both hexadecimal and decimal
-for hexDec in range(0, 99):
-    print("{} = {}".format(hexDec, hex(hexDec)))
+for num in range(0, 99):
+    print("{:d} = 0x{:x}".format(num, num))
