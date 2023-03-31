7e838c13923b62b4d718bcc237b781766e828aeb
SilGithinji <isaacgithinji4612@gmail.com>
2023-03-30 20:18:53 -0700

    A function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a function that prints a square with the # char """
+
+
+def print_square(size):
+    """ we print a size x size square out and are happy with it
+    Args:
+        size: the size of the square we ant printed
+    Return: no return
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end."") for j in range(size)]
+        print("")
