e8c3c992cfee94077d07d17ccdfbd1b12378ff42
SilGithinji <isaacgithinji4612@gmail.com>
2023-05-17 01:15:54 -0700

    SQL - More queries.  10. Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- To list all shows in (hbtn_0d_tvshows) that have at least one genre embedded and,
+-- all records are sorted in an ascending order based on tv_shows.title and tv_show_genres.genre_id --
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
