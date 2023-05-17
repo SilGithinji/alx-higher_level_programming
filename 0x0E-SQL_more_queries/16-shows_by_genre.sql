828a7eeb7f926fc6fcf14f123ea1656475b2a935
SilGithinji <isaacgithinji4612@gmail.com>
2023-05-17 01:36:05 -0700

    SQL - More queries.  16. List shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- To list all shows and genres linked to the show from (hbtn_0d_tvshows) and,
+-- all records are ordered in an ascending order based on show title and genre name --
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
