cccbe990698510083e1c6e2f319dd10c6437103f
SilGithinji <isaacgithinji4612@gmail.com>
2023-05-17 01:28:54 -0700

    SQL - More queries.  14. My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- To list all genres of the show (Dexter) in the database (hbtn_0d_tvshows) and,
+-- all records are ordered in an ascending order based on genre name --
+SELECT g.`name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON t.`id` = s.`show_id`
+       WHERE t.`title` = "Dexter"
+ ORDER BY g.`name`;
