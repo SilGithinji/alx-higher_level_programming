33ac0621c6f49ec9810c7e9289f010fc30e7aa1a
root <isaacgithinji4612@gmail.com>
2023-07-25 10:57:06 +0000

    Task 6.  A script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,24 +1,18 @@
 #!/usr/bin/node
-/** 
-*/
-const myArgs = process.argv.slice(2);
 const request = require('request');
-const id_ = [];
-const result = {};
-request(myArgs[0], function (error, response, body) {
-  let i;
-  if (!error) {
-    const json_ = JSON.parse(body);
-    const len = json_.length;
-    for (i = 0; i < len; i++) {
-      if (json_[i].completed) {
-        id_.push(json_[i].userId);
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const resp = {};
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (resp[json[i].userId] === undefined) {
+          resp[json[i].userId] = 0;
+        }
+        resp[json[i].userId]++;
       }
     }
-    for (let j = 0; j < id_.length; ++j) {
-      if (!result[id_[j]]) { result[id_[j]] = 0; }
-      ++result[id_[j]];
-    }
+    console.log(resp);
   }
-  console.log(result);
 });
